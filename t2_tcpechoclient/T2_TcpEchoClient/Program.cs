5690e5fdec0757277467064a89ec1c043067794d
Nikita Abrashnev <Nikita Abrashnev>
2016-12-09 01:26:30 +0300

    Initial

---
+++ t2_tcpechoclient/T2_TcpEchoClient/Program.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * User: Garraty
+ * Date: 15.09.2015
+ * Time: 17:16
+ * 
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+using System;							//For string, Int32, Console, ArgumentException
+using System.Text; 				//For Encoding
+using System.IO;					//For IOException 
+using System.Net.Sockets;	//For TcpClient, NewtworkStream, SocketException
+
+namespace T2_TcpEchoClient
+{
+	class TcpEchoClient
+	{
+		public static void Main(string[] args)
+		{
+			if (args.Length < 2 || args.Length > 3) 
+			{
+				//test for correct # of args
+				throw new ArgumentException("Parameters: <Server> <Word> [<Port>]");
+			}
+			
+			string server = args[0]; // Server name of IP address
+			
+			//Convert input string to bytes
+			byte[] byteBuffer = Encoding.ASCII.GetBytes(args[1]);
+			
+			//Use port argument if supplied, otherwise default to 7
+			int servPort = (args.Length == 3) ? Int32.Parse(args[2]) : 7;
+			
+			TcpClient client = null;
+			NetworkStream netStream = null;
+			
+			try
+			{
+				//Create socket that is connected to server on specified port
+				client = new TcpClient();
+				client.Connect(server, servPort);
+				
+				Console.WriteLine("Connected to server... sending echo string");
+				
+				netStream = client.GetStream();
+				
+				//Send the echoed string to the server
+				netStream.Write(byteBuffer, 0, byteBuffer.Length);
+				
+				Console.WriteLine("Sent {0} bytes to server...", byteBuffer.Length);
+				
+				int totalBytesRcvd = 0; //Total bytes recieved so far
+				int bytesRcvd = 0; 			//Bytes received in last read
+				
+				//Receive the same string back from the server
+				while (totalBytesRcvd < byteBuffer.Length)
+				{
+					if ((bytesRcvd = netStream.Read(byteBuffer, totalBytesRcvd,
+					                                byteBuffer.Length - totalBytesRcvd)) == 0)
+					{
+						Console.WriteLine("Connection closed prematurely.");
+						break;
+					}
+					totalBytesRcvd += bytesRcvd;
+				}
+				Console.WriteLine("Received {0} bytes from server: {1}", totalBytesRcvd, Encoding.ASCII.GetString(byteBuffer, 0, totalBytesRcvd));
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			finally
+			{
+				if (netStream != null)
+					netStream.Close();
+				if (client != null)
+					client.Close();
+			}
+			
+			Console.Write("\n\nPress any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ t3_tcpechoserver/T3_TcpEchoServer/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * User: Garraty
+ * Date: 17.09.2015
+ * Time: 20:46
+ * 
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+using System;							//For Console, Int32, ArgumentException, Enviroment
+using System.Net; 				//For IPAddress
+using System.Net.Sockets; //For TcpListener, TcpClient
+
+namespace T3_TcpEchoServer
+{
+	class TcpEchoServer
+	{
+		private const int BUFSIZE = 32; //Size of receive buffer
+		
+		public static void Main(string[] args)
+		{
+			if (args.Length > 1) //Test for correct # of args
+				throw new ArgumentException("Parameters: [<Port>]");
+			
+			int servPort = (args.Length == 1) ? Int32.Parse(args[0]) : 7;
+			
+			TcpListener listener = null;
+			
+			try
+			{
+				//Create TcpListener to accept client connections
+				IPAddress newIP;
+				//newIP = IPAddress.Any;
+				newIP = IPAddress.Parse("127.0.0.1");
+				Console.WriteLine("Starting server on IP - {0}:{1}", newIP.ToString(), servPort);
+				listener = new TcpListener(newIP, servPort);
+				listener.Start();
+			}
+			catch (SocketException se)
+			{
+				Console.WriteLine(se.ErrorCode + " : " + se.Message);
+				Environment.Exit(se.ErrorCode);
+			}
+			
+			byte[] rcvBuffer = new byte[BUFSIZE]; 	// Receive buffer
+			int bytesRcvd;													// Received bytes count
+			for (;;)
+			{
+				//Run forever, accepting and servicing connections (I bless you)
+				TcpClient client = null;
+				NetworkStream netStream = null;
+				
+				try
+				{
+					Console.WriteLine("Waiting for the client to connect...");
+					client = listener.AcceptTcpClient(); //Get client connection
+					netStream = client.GetStream();
+					Console.Write("Handling client - ");
+					
+					//Receive until client closes connection, indicated by 0 return value
+					int totalBytesEchoed = 0;
+					while ((bytesRcvd = netStream.Read(rcvBuffer, 0, rcvBuffer.Length)) > 0)
+					{
+						netStream.Write(rcvBuffer, 0, bytesRcvd);
+						totalBytesEchoed += bytesRcvd;
+					}
+					
+					Console.WriteLine("echoed {0} bytes.", totalBytesEchoed);
+					
+					//Close the stream and socket. We are done with this Client!
+					netStream.Close();
+					client.Close();
+				}
+				
+				catch (Exception e)
+				{
+					Console.WriteLine("Error: {1}", e.Message);
+					netStream.Close();
+				}
+			}
+		}
+	}
+}
